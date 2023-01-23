7c29cce134784b33d5626f25366b2f746f1ca70f
Rachel Drake <rachel.drake@sharper-pencil.co.uk>
2023-01-23 15:54:45 +0000

    Use handlebars.
    Fix tailwind content config.
    Rearrange and rephrase some content.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 import gulp from "gulp";
 import { deleteAsync as del } from "del";
+import hb from "gulp-hb";
 import postCss from "gulp-postcss";
 import autoprefixer from "autoprefixer";
 import tailwind from "tailwindcss";
@@ -15,14 +16,21 @@ const siteRoot = "./src";
 const distRoot = "./dist";
 
 const assetsRoot = "/assets";
+const handlebarsRoot = siteRoot + "/handlebars";
+
+const destHtmlFolder = distRoot;
 const destStylesFolder = distRoot + assetsRoot + "/css";
 const destScriptsFolder = distRoot + assetsRoot + "/js";
-const srcFiles = [siteRoot + "/**/*.html", siteRoot + assetsRoot + "/images/**/*.*"];
+
+const srcFiles = siteRoot + assetsRoot + "/images/**/*.*";
+const srcHtml = siteRoot + "/**/*.html";
 const srcStyles = siteRoot + assetsRoot + "/css/**/*.*";
 const srcScripts = siteRoot + assetsRoot + "/js/**/*.*";
+
 const styleEntryPoints = siteRoot + assetsRoot + "/css/style.css";
 const scriptEntryPoints = siteRoot + assetsRoot + "/js/*.js";
-const cssOutputFiles = [siteRoot + "/**/*.html", srcScripts];
+
+const cssOutputFiles = [siteRoot + "/**/*.html", handlebarsRoot + "/**/*.*", srcScripts];
 
 // Tasks
 const clean = () =>
@@ -32,16 +40,17 @@ const copyFiles = () => gulp
     .src(srcFiles, { base: siteRoot })
     .pipe(gulp.dest(distRoot));
 
-const processCss = inProd => {
-    let task = () => gulp
-        .src(styleEntryPoints)
-        .pipe(postCss([tailwind({ purge: { enabled: inProd, content: cssOutputFiles } }), autoprefixer]))
-        .pipe(gulp.dest(destStylesFolder))
-        .pipe(postCss([csso]))
-        .pipe(gulp.dest(destStylesFolder + "/min"));
-    Object.assign(task, { displayName: "processCss" });
-    return task;
-};
+const processHtml = () => gulp
+    .src(srcHtml, { base: siteRoot })
+    .pipe(hb().partials(handlebarsRoot + "/partials/**/*.hbs"))
+    .pipe(gulp.dest(destHtmlFolder));
+
+const processCss = () => gulp
+    .src(styleEntryPoints)
+    .pipe(postCss([tailwind({ content: cssOutputFiles }), autoprefixer]))
+    .pipe(gulp.dest(destStylesFolder))
+    .pipe(postCss([csso]))
+    .pipe(gulp.dest(destStylesFolder + "/min"));
 
 const processJs = inProd => {
     let task = () => gulp
@@ -77,12 +86,13 @@ const reloadServer = done => {
 
 // Watchers
 const watchSrc = () => gulp.watch(srcFiles, { verbose: true }, gulp.series(copyFiles, reloadServer));
-const watchCss = () => gulp.watch([srcStyles].concat(cssOutputFiles), { verbose: true }, gulp.series(processCss(false), reloadServer));
+const watchHtml = () => gulp.watch([srcHtml, handlebarsRoot + "/**/*.*"], { verbose: true }, gulp.series(processHtml, reloadServer));
+const watchCss = () => gulp.watch([srcStyles].concat(cssOutputFiles), { verbose: true }, gulp.series(processCss, reloadServer));
 const watchJs = () => gulp.watch(srcScripts, { verbose: true }, gulp.series(processJs(false), reloadServer));
 
 // Sequences
-const frontend = inProd => gulp.parallel(copyFiles, processCss(inProd), processJs(inProd));
-const watch = gulp.parallel(watchSrc, watchCss, watchJs);
+const frontend = inProd => gulp.parallel(copyFiles, processHtml, processCss, processJs(inProd));
+const watch = gulp.parallel(watchSrc, watchHtml, watchCss, watchJs);
 const buildDev = gulp.series(clean, frontend(false));
 const buildProd = gulp.series(frontend(true));
 
